764e08eb1953a19623f5f87c7aaad2de4fa855b5
Heet Bhimani <34266111+heetu@users.noreply.github.com>
2019-01-29 19:36:23 +0530

    Add files via upload

---
+++ HelloClient.java
@@ -0,0 +1,13 @@
+import java.rmi.Naming;
+
+public class HelloClient
+{
+public static void main(String[] argv) {
+try {
+HelloInterface hello =(HelloInterface) Naming.lookup ("//localhost/Hello");
+System.out.println (hello.say());
+    } 
+	catch (Exception e){
+		System.out.println ("HelloClient exception: " + e);}
+  }
+}
